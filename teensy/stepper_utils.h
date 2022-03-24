155ef4ad2ff048db4fd4f7eba71158f2aa9992c5
CloudHead84 <101837284+CloudHead84@users.noreply.github.com>
2022-03-24 08:07:49 +0100

    Update stepper_utils.h

--- teensy/stepper_utils.h
+++ teensy/stepper_utils.h
@@ -6,16 +6,48 @@ struct REF_AXIS {
     bool IN_xStartReq;
     bool IN_xStopReq;     
     int IN_iModeReq;   
-    int IN_iTargetPosition;
-    int IN_iTargetSpeed;
-    int IN_iTargetAcc;
-    int IN_iTargetDec;
+    long IN_diTargetPosition;   
+    float IN_rTargetSpeed;
+    float IN_rTargetAcc;
+    float IN_rTargetDec;
 
     //OUT
     bool OUT_xLifeBitFB;    
     bool OUT_xPowered;
     bool OUT_xRunning;   
-    int OUT_iActualPosition;
-    int OUT_iActualSpeed;   
+    long OUT_diActualPosition;  
+    float OUT_rActualSpeed;   
     int OUT_iCheckSum;   
 } ; 
+
+//-----------------------------------------------------------------------------
+// Concatenate two words into an long
+//-----------------------------------------------------------------------------
+long create_long(word low, word high)
+{
+    long result;
+    result = (long)(high << 16) | (long)low;
+    return result;
+}
+
+//-----------------------------------------------------------------------------
+// deConcatenate long into two words
+//-----------------------------------------------------------------------------
+long create_words(long in, bool selection)
+{
+    word result;
+    long tmpLong;
+
+    if (selection == false)
+    {
+      tmpLong = (in << 16);
+      tmpLong = (tmpLong >> 16);
+      result = (word)tmpLong;
+    }
+    else
+    {
+      tmpLong = (in >> 16);
+      result = (word)tmpLong;
+    }
+    return result;
+}
